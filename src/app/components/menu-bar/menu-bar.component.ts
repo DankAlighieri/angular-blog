df9583edc66a63949977f5cdcefde726ef7cd8aa
Guilherme <guilhermeemeterioif@gmail.com>
2023-09-26 21:58:06 -0300

    new elements

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
 
-  homeIcon:string = '../../../assets/icons8-home-48.png'
+  homeIcon:string = '../../../assets/icons8-home-48.png';
+  linkedinIcon:string = '../../../assets/icons8-linkedin-50.png';
+  githubIcon:string='../../../assets/icons8-github-50.png';
 
   constructor() { }
 
